1a30edb132b70a9ebf9653b55104aeae452e5479
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2023-02-11 22:22:32 +0200

    update files

--- src/pages/upload.tsx
+++ src/pages/upload.tsx
@@ -2,24 +2,24 @@ import { Box, FormControl, InputLabel, MenuItem, Select, Typography } from '@mui
 import React, { useState } from 'react'
 import { countries } from "../utils/countries"
 const Upload = () => {
-  const [countryCode, setCountryCode] = useState<string>()
-  return (
-    <Box sx={{ textAlign: "center" }} mt={12}>
-      <Typography textAlign="center" variant="h3" gutterBottom>Create link for sharing</Typography>
-      <FormControl fullWidth sx={{ maxWidth: "30rem" }}>
-        <InputLabel id="country-select">Country</InputLabel>
-        <Select
-          labelId="country-select"
-          id="country-select"
-          value={countryCode}
-          label="Country"
-          onChange={(e) => setCountryCode(e.target.value)}
-        >
-          {countries.map(country => <MenuItem value={country.code}>{country.name}</MenuItem>)}
-        </Select>
-      </FormControl>
-    </Box>
-  )
+    const [countryCode, setCountryCode] = useState<string>()
+    return (
+        <Box sx={{ textAlign: "center" }} mt={12}>
+            <Typography mx="auto" variant="h3" gutterBottom>Create link for sharing</Typography>
+            <FormControl fullWidth sx={{ maxWidth: "30rem" }}>
+                <InputLabel id="country-select">Country</InputLabel>
+                <Select
+                    labelId="country-select"
+                    id="country-select"
+                    value={countryCode}
+                    label="Country"
+                    onChange={(e) => setCountryCode(e.target.value)}
+                >
+                    {countries.map(country => <MenuItem value={country.code}>{country.name}</MenuItem>)}
+                </Select>
+            </FormControl>
+        </Box>
+    )
 }
 
 export default Upload
