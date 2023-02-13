f49acb418e8a9ada291b59a87c4cdee6557db644
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2023-02-13 13:19:51 +0200

    add claim page

--- src/pages/claim.tsx
+++ src/pages/claim.tsx
@@ -1,15 +1,92 @@
-import { Typography } from '@mui/material'
-import React, { useEffect } from 'react'
+import { Alert, AlertTitle, Backdrop, Link, Box, Card, CardContent, Chip, CircularProgress, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
+import { ShareReward } from '../utils/types';
+import WalletEnsure from '../components/walletEnsure';
+import { countries } from '../utils/countries';
 
 const Claim = () => {
-  const { id } = useParams();
-  useEffect(() => {
-    // fetch donation using that id from Contract/BE
-  }, [])
-  return (
-    <Typography textAlign="center" variant="h3" gutterBottom>Claim from donation {id}</Typography>
-  )
+    const { id } = useParams();
+    const [shareReward, setShareReward] = useState<ShareReward>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [hasError, setHasError] = useState(false);
+    useEffect(() => {
+        (async () => {
+            setIsLoading(true);
+            try {
+                await setTimeout(() => {
+                    setShareReward({
+                        maxUsers: 12,
+                        countries: ["DE", "GB"],
+                        rewardAmount: 23852,
+                        minAge: 17,
+                        maxAge: 39,
+                        link: "https://www.google.com/"
+                    });
+                    setIsLoading(false);
+                }, 3000)
+
+            }
+            catch {
+                setHasError(true)
+            }
+        })()
+    }, []);
+    if (hasError)
+        return (
+            <WalletEnsure>
+                <Alert severity="error" sx={{ my: 8 }}>
+                    <AlertTitle>Claim link unavalaible</AlertTitle>
+                    Sadly, this link is unavalaible
+                </Alert>
+            </WalletEnsure>)
+    if (isLoading)
+        return (
+            <WalletEnsure>
+                <Box sx={{ mt: "30%", textAlign: "center" }}>
+                    <CircularProgress color="warning" size={100} />
+                </Box>
+            </WalletEnsure>
+        )
+    return (
+        <WalletEnsure>
+            <>
+                <Typography textAlign="center" variant="h3" marginTop={4} gutterBottom>
+                    Claim reward
+                </Typography>
+                <Card variant="outlined" sx={{ maxWidth: "35rem", mx: "auto" }}>
+                    <CardContent>
+                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                            Avalaible in
+                        </Typography>
+                        {shareReward?.countries.map((country, i) => (<Chip
+                            sx={{ margin: 1 }}
+                            size="small"
+                            variant="outlined"
+                            label={countries.find(e => e.code === country)?.name}
+                        />))}
+                        <Box my={2}>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary">
+                                Link to claim:
+                            </Typography>
+                            <Box sx={{
+                                border: '2px solid #343434',
+                                borderRadius: "10px",
+                                textAlign: "center",
+                                mx: "2em",
+                                padding: "0.5em"
+                            }}>
+                                <Link href={shareReward?.link} target="_blank">{shareReward?.link}</Link>
+                            </Box>
+                        </Box>
+                        <Typography variant="h5">
+                            Claimed {(Number(shareReward?.rewardAmount) / (shareReward?.maxUsers ?? 1)).toFixed(2)} CCD
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </>
+        </WalletEnsure>
+    )
 }
 
 export default Claim
--- src/pages/upload.tsx
+++ src/pages/upload.tsx
@@ -2,6 +2,7 @@ import { Alert, Link, Box, Button, Chip, FormControl, InputAdornment, InputLabel
 import React, { useState } from 'react'
 import { countries } from "../utils/countries"
 import { ShareReward } from '../utils/types';
+import WalletEnsure from '../components/walletEnsure';
 const Upload = () => {
     const [shareReward, setShareReward] = useState<ShareReward>(
         {
@@ -17,78 +18,80 @@ const Upload = () => {
             1000)
     }
     return (
-        <Box sx={{ textAlign: "center" }} mt={12}>
-            <Typography mx="auto" mb={8} variant="h4" gutterBottom>Create link for sharing reward</Typography>
-            <Box sx={{ maxWidth: "30rem" }} mx="auto">
-                <Typography variant="h5" my={2} textAlign="left">Countries</Typography>
-                <FormControl fullWidth >
-                    <InputLabel id="country-select">Country</InputLabel>
-                    <Select
-                        labelId="country-select"
-                        id="country-select"
-                        multiple
-                        value={shareReward?.countries ?? []}
-                        label="Countries"
-                        onChange={(e: SelectChangeEvent<string[]>) => {
-                            const {
-                                target: { value },
-                            } = e;
-                            setShareReward({ ...shareReward, countries: typeof value === 'string' ? value.split(',') : value })
-                        }
-                        }
+        <WalletEnsure>
+            <Box sx={{ textAlign: "center" }} mt={12}>
+                <Typography mx="auto" mb={8} variant="h4" gutterBottom>Create link for sharing reward</Typography>
+                <Box sx={{ maxWidth: "30rem" }} mx="auto">
+                    <Typography variant="h5" my={2} textAlign="left">Countries</Typography>
+                    <FormControl fullWidth >
+                        <InputLabel id="country-select">Country</InputLabel>
+                        <Select
+                            labelId="country-select"
+                            id="country-select"
+                            multiple
+                            value={shareReward?.countries ?? []}
+                            label="Countries"
+                            onChange={(e: SelectChangeEvent<string[]>) => {
+                                const {
+                                    target: { value },
+                                } = e;
+                                setShareReward({ ...shareReward, countries: typeof value === 'string' ? value.split(',') : value })
+                            }
+                            }
+                        >
+                            {countries.map(country => <MenuItem value={country.code}>{country.name}</MenuItem>)}
+                        </Select>
+                    </FormControl>
+
+                    {shareReward?.countries.map((country, i) => (<Chip
+                        sx={{ margin: 1 }}
+                        label={countries.find(e => e.code === country)?.name}
+                        onDelete={() => {
+                            const tCountries = shareReward.countries ?? [];
+                            tCountries.splice(i, 1);
+                            setShareReward({ ...shareReward, countries: tCountries });
+                        }}
+                    />))}
+
+                    <Typography variant="h5" my={2} textAlign="left">Ages</Typography>
+                    <Stack
+                        justifyContent="center"
+                        my={3}
+                        direction={{ xs: 'column', sm: 'row' }}
+                        spacing={{ xs: 1, sm: 2, md: 4 }}
                     >
-                        {countries.map(country => <MenuItem value={country.code}>{country.name}</MenuItem>)}
-                    </Select>
-                </FormControl>
+                        <TextField label="Min. age" inputProps={{ inputMode: 'numeric', pattern: '/[^0-9\.]+/g' }} fullWidth />
+                        <TextField label="Max age" inputProps={{ inputMode: 'numeric', pattern: '/[^0-9\.]+/g' }} fullWidth />
+                    </Stack>
 
-                {shareReward?.countries.map((country, i) => (<Chip
-                    sx={{ margin: 1 }}
-                    label={countries.find(e => e.code === country)?.name}
-                    onDelete={() => {
-                        const tCountries = shareReward.countries ?? [];
-                        tCountries.splice(i, 1);
-                        setShareReward({ ...shareReward, countries: tCountries });
-                    }}
-                />))}
+                    <Typography variant="h5" my={2} textAlign="left">Reward amount</Typography>
+                    <TextField InputProps={{
+                        startAdornment: <InputAdornment position="start">CCD</InputAdornment>,
+                    }} label="Reward Amount" inputProps={{ inputMode: 'numeric', pattern: '/[^0-9\.]+/g' }} fullWidth />
 
-                <Typography variant="h5" my={2} textAlign="left">Ages</Typography>
-                <Stack
-                    justifyContent="center"
-                    my={3}
-                    direction={{ xs: 'column', sm: 'row' }}
-                    spacing={{ xs: 1, sm: 2, md: 4 }}
-                >
-                    <TextField label="Min. age" inputProps={{ inputMode: 'numeric', pattern: '/[^0-9\.]+/g' }} fullWidth />
-                    <TextField label="Max age" inputProps={{ inputMode: 'numeric', pattern: '/[^0-9\.]+/g' }} fullWidth />
-                </Stack>
+                    <Typography variant="h5" my={2} textAlign="left">No. of users able to claim: {shareReward.maxUsers}</Typography>
+                    <Slider
+                        color="primary"
+                        aria-label="Max Users"
+                        value={shareReward?.maxUsers}
+                        onChange={(event: Event, newValue: number | number[]) => {
+                            setShareReward({ ...shareReward!, maxUsers: newValue as number })
+                        }}
+                        valueLabelDisplay="auto"
+                        step={1}
+                        marks
+                        min={1}
+                        max={24}
+                    />
+                    {shareReward.rewardAmount && (<Typography variant="body2" textAlign="right" color="gray">{Number(shareReward.rewardAmount) / (shareReward.maxUsers ?? 1)} CCD for each user</Typography>)}
+                    <Button onClick={() => generateLink()} variant="contained" color="warning" sx={{ marginY: 4 }}>
+                        Generate reward link
+                    </Button>
 
-                <Typography variant="h5" my={2} textAlign="left">Reward amount</Typography>
-                <TextField InputProps={{
-                    startAdornment: <InputAdornment position="start">CCD</InputAdornment>,
-                }} label="Reward Amount" inputProps={{ inputMode: 'numeric', pattern: '/[^0-9\.]+/g' }} fullWidth />
-
-                <Typography variant="h5" my={2} textAlign="left">No. of users able to claim</Typography>
-                <Slider
-                    color="warning"
-                    aria-label="Max Users"
-                    value={shareReward?.maxUsers}
-                    onChange={(event: Event, newValue: number | number[]) => {
-                        setShareReward({ ...shareReward!, maxUsers: newValue as number })
-                    }}
-                    valueLabelDisplay="auto"
-                    step={1}
-                    marks
-                    min={1}
-                    max={24}
-                />
-                {shareReward.rewardAmount && (<Typography variant="body2" textAlign="right" color="gray">{Number(shareReward.rewardAmount) / (shareReward.maxUsers ?? 1)} CCD for each user</Typography>)}
-                <Button onClick={() => generateLink()} variant="contained" color="warning" sx={{ marginY: 4 }}>
-                    Generate reward link
-                </Button>
-
-                {generatedLink && (<Alert sx={{ mb: 4 }} severity="success">Shareable link generated: <Link variant="inherit" href={generatedLink}>{generatedLink}</Link></Alert>)}
+                    {generatedLink && (<Alert sx={{ mb: 4 }} severity="success">Shareable link generated: <Link variant="inherit" href={generatedLink}>{generatedLink}</Link></Alert>)}
+                </Box>
             </Box>
-        </Box>
+        </WalletEnsure>
     )
 }
 
